a6eac0cc9452bf7239a49affe09ddfae24bf2e9e
sangamessshhh <138979376+sangamessshhh@users.noreply.github.com>
2023-07-15 23:19:09 +0530

    Update mod3.sol

--- mod3.sol
+++ mod3.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Sangamesh is ERC20, Ownable {
+    constructor() ERC20("Sangamesh", "SMG") {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public {
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+        _burn(msg.sender, amount);
+    }
+}
